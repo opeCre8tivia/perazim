2134312a6b9913de8c4b44c52e2b5d92c2cbd7ad
opeCre8tivia <opegude.n3t@gmail.com>
2022-09-08 20:34:05 +0300

    adding framer motion to the project

--- src/Components/MenuBar/MenuBar.tsx
+++ src/Components/MenuBar/MenuBar.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const MenuBar = () => {
 
   return (
 
-    <div className='w-full h-[100px] flex items-center px-10 bg-pink-500 font-bold text-green-300' >
+    <motion.div drag className='w-full h-[100px] flex items-center px-10 bg-pink-500 font-bold text-green-300' >
             MENU BAR
-    </div>
+    </motion.div>
   )
 }
 
