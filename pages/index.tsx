4158aacdf48d6ecdac05ea574d347d60837f5737
opeCre8tivia <opegude.n3t@gmail.com>
2023-03-18 16:12:46 +0300

    removing blockquote element

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,13 +57,13 @@ const Home: NextPage = () => {
                     fill="currentColor"
                   />
                 </svg>
-                <blockquote>
-                  <p className="text-2xl font-medium text-gray-900 dark:text-white">
-                    "Perazim is just awesome. It contains tons of predesigned
-                    components and pages starting from login screen to complex
-                    dashboard. Perfect choice for your next SaaS application."
-                  </p>
-                </blockquote>
+
+                <p className="text-2xl font-medium text-gray-900 dark:text-white">
+                  Perazim is just awesome. It contains tons of predesigned
+                  components and pages starting from login screen to complex
+                  dashboard. Perfect choice for your next SaaS application
+                </p>
+
                 <figcaption className="flex items-center justify-center mt-6 space-x-3">
                   <img
                     className="w-6 h-6 rounded-full"
