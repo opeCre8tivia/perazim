ac33ae4c8a00a675e4d364e2443b3e6983366d38
opeCre8tivia <opegude.n3t@gmail.com>
2023-05-18 01:57:39 +0300

    fix img bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ import { ReactComponent as CompanyIcon } from "../public/perazimlogo.svg";
 //@ts-ignore
 import { WhatsAppWidget } from "react-whatsapp-widget";
 import "react-whatsapp-widget/dist/index.css";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   const [showMobileNav, setShowMobileNav] = useState<boolean>(false);
@@ -85,10 +86,12 @@ const Home: NextPage = () => {
                 </p>
 
                 <figcaption className="flex items-center justify-center mt-6 space-x-3">
-                  <img
+                  <Image
                     className="w-6 h-6 rounded-full"
                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
                     alt="profile picture"
+                    width={50}
+                    height={50}
                   />
                   <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
                     <div className="pr-3 font-medium text-gray-900 dark:text-white">
